d9363fb04a4ae91559d102456308580d027cebd7
Theo <theosmithscott@protonmail.com>
2022-05-11 01:15:57 +0100

    Created page for multiple choice game
    - Added article to the homepage
    - Created javascript file for multiple choice game logic (multiplechoice.js) -- NOT WORKING

---
+++ website/static/blog/multiplechoice.js
@@ -0,0 +1,20 @@
+const intros = [
+    ["Intro1Answer", 4],
+    ["Intro2Answer", 1],
+    ["Intro3Answer", 2],
+    ["Intro4Answer", 3]]
+
+
+function addQuestionDiv(text) {
+    const article = document.getElementById("multiplechoice-article");
+
+}
+
+function addAnswers() {
+    for(const answer in intros) {
+
+    }
+}
+
+addQuestionDiv()
+addAnswers()
